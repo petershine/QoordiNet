5ec75cda68ab6d3b1a68e046ebd9244a8c5b0e0d
petershine <petershine@fxceed.com>
2023-12-28 01:18:44 -0500

    csv file uploading and display as HTML <table>, using Pandas

--- mainApp/app_constants.py
+++ mainApp/app_constants.py
@@ -8,5 +8,7 @@ PORT_NUMBER = 8083
 HTML_TEMPLATE_DIRECTORY = "mainApp/templates"
 HTML_TEMPLATE_FORM = "form.html"
 HTML_TEMPLATE_TABLE = "table.html"
+HTML_TEMPLATE_UPLOAD = "upload.html"
+HTML_TEMPLATE_DISPLAY_CSV = "display_csv.html"
 
 HTML_STATIC_DIRECTORY = "mainApp/static"
--- mainApp/interfaces.py
+++ mainApp/interfaces.py
@@ -2,10 +2,13 @@ from ._shared import constants
 from . import app_constants
 
 from fastapi import FastAPI, Form, Request
+from fastapi import File, UploadFile
 from fastapi.responses import HTMLResponse, RedirectResponse
 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+import pandas as pd
 
 
 webAppManager = None
@@ -27,3 +30,13 @@ async def read_form(request: Request):
 async def display_data(request: Request, tab_separated: str = Form(...)):
     data = webAppManager.process_data(tab_separated)
     return templates.TemplateResponse(app_constants.HTML_TEMPLATE_TABLE, {"request": request, "data": data})
+
+@app.get("/upload", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse(app_constants.HTML_TEMPLATE_UPLOAD, {"request": request})
+
+@app.post("/display_csv/", response_class=HTMLResponse)
+async def create_upload_file(request: Request, csv_file: UploadFile = File(...)):
+    df = pd.read_csv(csv_file.file)
+    html_table = df.to_html(classes="csv_table")
+    return templates.TemplateResponse(app_constants.HTML_TEMPLATE_DISPLAY_CSV, {"request": request, "html_table": html_table})
