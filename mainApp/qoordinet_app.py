b3bb9daad7d2d5195157444b95820944be1a3839
petershine <petershine@fxceed.com>
2024-06-10 17:11:24 -0400

    change evaluation condition

--- mainApp/qoordinet_app.py
+++ mainApp/qoordinet_app.py
@@ -154,7 +154,7 @@ class QoordiNetAppManager(BaseApp):
 
         df = df[runDateKeyMask]
         df = df[actionKeyMask]
-        df = df[amountKeyMask & quantityKeyMask]
+        df = df[amountKeyMask or quantityKeyMask]
 
     
         df[typeColumnKey] = ''
