8e5703c5ebf85f90504f5c2dc042adb60ac2d2e3
petershine <petershine@fxceed.com>
2024-11-10 22:10:47 -0500

    added "deleteLast()" javascript function

---
+++ mainApp/static/deleteLast.js
@@ -0,0 +1,14 @@
+async function deleteLast(buttonElement) {
+    try {
+        showSpinner()
+        const response = await fetch('/delete_last', {
+            method: 'DELETE'
+        });
+        const jsonData = await response.json(); // This is your array of dictionaries
+        console.log(jsonData)
+    } catch (error) {
+        console.error('Fetch error:', error);
+    } finally {
+        hideSpinner()
+    }
+}
