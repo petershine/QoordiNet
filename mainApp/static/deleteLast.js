1c61ddb0833fc901fefe5cb53e21daa7344503b7
petershine <petershine@fxceed.com>
2024-11-24 01:05:14 -0500

    [javascript] redirect from fetched response

--- mainApp/static/deleteLast.js
+++ mainApp/static/deleteLast.js
@@ -2,9 +2,13 @@ async function deleteLast(buttonElement) {
     try {
         showSpinner()
         const response = await fetch('/delete_last', {
-            method: 'DELETE'
-        });
+            method: 'DELETE',
+            redirect: "follow"
+        })
         console.log(response)
+        if (response.redirected) {
+            window.location.href = response.url;
+        }
     } catch (error) {
         console.error('Fetch error:', error);
     } finally {
